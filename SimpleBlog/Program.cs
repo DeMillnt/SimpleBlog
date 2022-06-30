4ddf8f0e11c9cb6fdcf4ca727430eeea011583a9
iskander <iskandersitdikov121@gmail.com>
2022-06-30 11:43:43 +0600

    asp net init

---
+++ SimpleBlog/Program.cs
@@ -0,0 +1,27 @@
+using Application.Interfaces;
+using Infrastructure.Context;
+using Infrastructure.MapProfiles;
+using Infrastructure.Seed;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
